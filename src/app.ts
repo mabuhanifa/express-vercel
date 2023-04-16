20e23e5476c62a927975c7e8001a954cf622a5d3
shourovahmd <moh.abuhanifa@gmail.com>
2023-04-17 03:51:15 +0600

    npm i @types/mysql

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,18 @@
 import cors from "cors";
 import * as dotenv from "dotenv";
 import express, { Request, Response } from "express";
+import mysql from "mysql";
 dotenv.config();
 
 const app = express();
 app.use(express.json());
 app.use(cors());
 const port = process.env.PORT || 3000;
+const connectionString = process.env.DATABASE_URL || "";
+const connection = mysql.createConnection;
 
 app.get("/api/characters", (req: Request, res: Response) => {
+  const query = "Select * from Characters";
   res.send("It working");
 });
 
