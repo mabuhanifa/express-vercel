21f4063a947758b4433e877d8a8f4f8ded7715d6
mabuhanifa <moh.abuhanifa@gmail.com>
2025-04-11 15:41:02 +0600

    port added

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
 const express = require("express");
 const app = express();
+const port = process.env.PORT || 3000;
 
-// Middleware and routes
 app.get("/", (req, res) => {
   res.json({ message: "Hello from Vercel!" });
 });
 
-// Export as a Vercel serverless function
+app.listen(port, () => {
+  console.log("Server is running on port 3000");
+});
+
 module.exports = (req, res) => {
-  // This handles the Vercel lambda invocation
   app(req, res);
 };
