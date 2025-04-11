9b469d27215b5538940533410f1d11175f477efe
mabuhanifa <moh.abuhanifa@gmail.com>
2025-04-11 16:01:52 +0600

    post api created

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT || 3000;
 const blogRoute = require("./routes/blog.route");
-
+app.use(express.json());
 app.use("/blog", blogRoute);
 
 app.get("/", (req, res) => {
--- routes/blog.route.js
+++ routes/blog.route.js
@@ -2,8 +2,13 @@ const { Router } = require("express");
 
 const router = Router();
 
-router.get("/", (req, res) => {
-  res.send("Blog route");
-});
+router
+  .get("/", (req, res) => {
+    res.json({ message: "Blog route" });
+  })
+  .post("/", (req, res) => {
+    console.log(req.body);
+    res.json({ message: req.body });
+  });
 
 module.exports = router;
